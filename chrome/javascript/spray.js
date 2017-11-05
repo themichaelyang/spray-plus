527d2672bba712509430dac0f304be4ea2543e08
Michael Yang <themichaelyang@gmail.com>
2017-11-05 01:03:58 -0400

    spray on facebook and youtube

--- chrome/javascript/spray.js
+++ chrome/javascript/spray.js
@@ -1,12 +1,15 @@
 const socket = io('https://localhost:8080/')
 
-function spray() {
-  socket.emit('spray', true)
-  console.log('spray')
+let spray = () => {
+  if (socket) {
+    socket.emit('spray', true)
+    console.log('spray')
+  }
 }
 
 chrome.storage.sync.get('on', (response) => {
   if (response.on) {
-    spray()
+    // spray once every 2 seconds
+    setInterval(spray, 2000)
   }
 })
