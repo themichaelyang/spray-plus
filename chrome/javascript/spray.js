ef56def4b8c56d5b01fb30291b73b8c6aa914ffc
Michael Yang <themichaelyang@gmail.com>
2017-11-05 00:56:04 -0400

    added spray on signal

--- chrome/javascript/spray.js
+++ chrome/javascript/spray.js
@@ -1,8 +1,12 @@
+const socket = io('https://localhost:8080/')
+
+function spray() {
+  socket.emit('spray', true)
+  console.log('spray')
+}
+
 chrome.storage.sync.get('on', (response) => {
   if (response.on) {
-    const socket = io('https://localhost:8080/')
-    socket.emit('spray', true)
-    console.log('spray')
-    console.log(option)
+    spray()
   }
 })
--- server/server.js
+++ server/server.js
@@ -1,33 +1,57 @@
 const fs = require('fs')
 const https = require('https')
+const five = require('johnny-five')
+const board = new five.Board()
+const sleep = require('sleep').msleep
+
 const options = {
   key: fs.readFileSync(__dirname + '/ssl/server.pem'),
   cert: fs.readFileSync(__dirname + '/ssl/server.crt')
 }
 
-const server = https.createServer(options, (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('okay')
-})
+let start = 0
+let spray = (servo, degrees) => {
+  servo.to(degrees)
+  sleep(800)
+  servo.to(start)
+}
 
-const io = require('socket.io')(server)
-const port = 8080
-
-io.on('connection', (socket) => {
-  console.log('Connected')
-  socket.on('disconnect', () => {
-    console.log('Disconnected')
-  });
-
-  socket.on('spray', () => {
-    console.log('SPRAY ***')
+board.on('ready', function() {
+  let pin = 9
+  let servo = new five.Servo({
+    pin: 9,
+    startAt: start
   })
-})
 
-server.listen(port, () => {
-  console.log('listening on https://localhost:' + port)
+  this.repl.inject({
+    servo: servo
+  })
 
-  require('dns').lookup(require('os').hostname(), (err, add, fam) => {
-    console.log('https://' +add + ':' + port)
+  const server = https.createServer(options, (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('okay')
+  })
+
+  const io = require('socket.io')(server)
+  const port = 8080
+
+  io.on('connection', (socket) => {
+    console.log('Connected')
+    socket.on('disconnect', () => {
+      console.log('Disconnected')
+    });
+
+    socket.on('spray', () => {
+      console.log('SPRAY ***')
+      spray(servo, 120)
+    })
+  })
+
+  server.listen(port, () => {
+    console.log('listening on https://localhost:' + port)
+
+    require('dns').lookup(require('os').hostname(), (err, add, fam) => {
+      console.log('https://' +add + ':' + port)
+    })
   })
 })
